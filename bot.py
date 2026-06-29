8bab7bcbcea8140bf5807822c7f3d0bdd219fddd
dfof1298-prog <dfof1298@gmail.com>
2026-06-29 13:21:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ import threading
 active_scans = {}
 
 stopuser = {}
-token = '8772097807:AAE23_1k3G9DdeC5jMlY2XB-o50L8Yga6Mo'
+token = '8772097807:AAGcAAQ5cxNJmXaRKyFsZvIdP_gqmkaTk1U'
 bot=telebot.TeleBot(token,parse_mode="HTML")
 admin= 1093032296
 f = Faker()
