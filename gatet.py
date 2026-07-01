83f8baaf04960b501263bcb84e6910818fec285c
dfof1298-prog <dfof1298@gmail.com>
2026-07-01 05:06:48 +0300

    Update gatet.py

--- gatet.py
+++ gatet.py
@@ -268,8 +268,8 @@ def brn6(ccx):
 
 
 # ==================== إعدادات الموقع الجديد (Oking Foundation) ====================
-SITE_URL = 'https://donatebear.com/donations/empower-the-kids-of-ghana-by-volunteering-causes/'
-URL_AJAX = 'https://donatebear.com/wp-admin/admin-ajax.php'
+SITE_URL = 'https://www.theflorentine.net/support-the-florentine/'
+URL_AJAX = 'https://www.theflorentine.net/wp-admin/admin-ajax.php'
 
 def extract_data():
     s = requests.Session()
